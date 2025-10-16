d9c92456059e5c556828bcb4b2b98c09c494b2ce
gustavosouto02 <gugasouto2004@gmail.com>
2025-10-16 15:05:12 -0300

    initial commit

---
+++ EventKitApp/EventKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventKitAppApp.swift
+//  EventKitApp
+//
+//  Created by Gustavo Souto Pereira on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EventKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
